2436fcff027d0c8b26b6a1097cfbf45c3c1ef96e
chandragunda9 <chandragunda1215@gmail.com>
2024-09-02 23:07:27 +0530

    recursion

--- src/t6_recursion/faq_hard/SudokuSolver.java
+++ src/t6_recursion/faq_hard/SudokuSolver.java
@@ -1,5 +1,7 @@
 package t6_recursion.faq_hard;
 
+import java.util.Arrays;
+
 public class SudokuSolver {
     public void solveSudoku(char[][] board) {
         int row = 0, col = 0;
@@ -27,7 +29,7 @@ public class SudokuSolver {
         for (int i = row; i < board.length; i++) {
             for (int j = col; j < board[0].length; j++) {
                 if (board[i][j] == '.') {
-                    for (char digit = '0'; digit <= '9'; digit++) {
+                    for (char digit = '1'; digit <= '9'; digit++) {
                         if (isPossible(board, i, j, digit)) {
                             board[i][j] = digit;
                             if (solve(board, i, j)) return true;
@@ -39,5 +41,60 @@ public class SudokuSolver {
             }
         }
         return false;
+    }
+
+    public void solveSudoku(int[][] sudoku) {
+        int row = 0, col = 0;
+        solveTheSudoku(sudoku, row, col);
+    }
+
+    boolean solveTheSudoku(int[][] board, int row, int col) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 0) {
+                    for (int digit = 1; digit <= 9; digit++) {
+                        if (isAllConditionsMet(board, i, j, digit)) {
+                            board[i][j] = digit;
+                            if (solveTheSudoku(board, i, j)) return true;
+                            board[i][j] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean isAllConditionsMet(int[][] grid, int row, int col, int digit) {
+        for (int i = 0; i < 9; i++) {
+            if (grid[row][i] == digit || grid[i][col] == digit)
+                return false;
+        }
+
+        int startRow = row / 3 * 3;
+        int startCol = col / 3 * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (grid[i][j] == digit)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        SudokuSolver obj = new SudokuSolver();
+        int[][] sudoku = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+        obj.solveSudoku(sudoku);
+        System.out.println(Arrays.deepToString(sudoku));
     }
 }
