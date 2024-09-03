65fb2795883960f08c35964f1f9a9058bfde9fd5
chandragunda9 <chandragunda1215@gmail.com>
2024-09-03 20:09:49 +0530

    recursion

--- src/t6_recursion/faq_hard/SudokuSolver.java
+++ src/t6_recursion/faq_hard/SudokuSolver.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 
 public class SudokuSolver {
     public void solveSudoku(char[][] board) {
-        int row = 0, col = 0;
-        solve(board, row, col);
+        solve(board);
     }
 
     boolean isPossible(char[][] grid, int row, int col, char digit) {
@@ -25,59 +24,19 @@ public class SudokuSolver {
         return true;
     }
 
-    boolean solve(char[][] board, int row, int col) {
-        for (int i = row; i < board.length; i++) {
-            for (int j = col; j < board[0].length; j++) {
+    boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == '.') {
                     for (char digit = '1'; digit <= '9'; digit++) {
                         if (isPossible(board, i, j, digit)) {
                             board[i][j] = digit;
-                            if (solve(board, i, j)) return true;
+                            if (solve(board)) return true;
                             board[i][j] = '.';
                         }
                     }
                     return false;
                 }
-            }
-        }
-        return false;
-    }
-
-    public void solveSudoku(int[][] sudoku) {
-        int row = 0, col = 0;
-        solveTheSudoku(sudoku, row, col);
-    }
-
-    boolean solveTheSudoku(int[][] board, int row, int col) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 0) {
-                    for (int digit = 1; digit <= 9; digit++) {
-                        if (isAllConditionsMet(board, i, j, digit)) {
-                            board[i][j] = digit;
-                            if (solveTheSudoku(board, i, j)) return true;
-                            board[i][j] = 0;
-                        }
-                    }
-                    return false;
-                }
-            }
-        }
-        return false;
-    }
-
-    static boolean isAllConditionsMet(int[][] grid, int row, int col, int digit) {
-        for (int i = 0; i < 9; i++) {
-            if (grid[row][i] == digit || grid[i][col] == digit)
-                return false;
-        }
-
-        int startRow = row / 3 * 3;
-        int startCol = col / 3 * 3;
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (grid[i][j] == digit)
-                    return false;
             }
         }
         return true;
@@ -96,5 +55,44 @@ public class SudokuSolver {
                 {0, 0, 5, 2, 0, 6, 3, 0, 0}};
         obj.solveSudoku(sudoku);
         System.out.println(Arrays.deepToString(sudoku));
+    }
+
+    public void solveSudoku(int[][] sudoku) {
+        solveTheSudoku(sudoku);
+    }
+
+    boolean solveTheSudoku(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 0) {
+                    for (int digit = 1; digit <= 9; digit++) {
+                        if (isAllConditionsMet(board, i, j, digit)) {
+                            board[i][j] = digit;
+                            if (solveTheSudoku(board)) return true;
+                            board[i][j] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static boolean isAllConditionsMet(int[][] grid, int row, int col, int digit) {
+        for (int i = 0; i < 9; i++) {
+            if (grid[row][i] == digit || grid[i][col] == digit)
+                return false;
+        }
+
+        int startRow = row / 3 * 3;
+        int startCol = col / 3 * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (grid[i][j] == digit)
+                    return false;
+            }
+        }
+        return true;
     }
 }
